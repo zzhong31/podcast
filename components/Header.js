cade57c1e152ed45fe0ea89c6b2476c919a9be4f
zhenyu zhong <genericname001@gmail.com>
2020-09-28 21:19:24 -0500

    barebones page

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import { Navbar } from 'reactstrap';
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar color='light expand' expand='md'></Navbar>
+    </div>
+  );
+};
+
+export default Header;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Header />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
